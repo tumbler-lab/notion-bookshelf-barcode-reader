969a28a30dfcb8c423df1a8f7739f6985745b7cf
meros <tanburako11@gmail.com>
2022-12-04 22:48:27 +0900

    dialogを開いているときはscanしないようにする

--- app/src/main/java/com/example/notionbookshelfbarcodereader/MainActivity.kt
+++ app/src/main/java/com/example/notionbookshelfbarcodereader/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         baseUrl("https://iss.ndl.go.jp/")
     }.build()
     private val service = retrofit.create(IssNdlService::class.java) // NOTE: javaのクラス情報を渡す必要があるため
+    private var isOpenDialog = false // dialogが開いているかのフラグ
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         codeScanner = CodeScanner(this, binding.previewView, ::onDetectCode)
 
         if (CameraPermission.hasPermission(this)) {
-            start()
+            startScanner()
         } else {
             launcher.launch(Unit)
         }
@@ -46,28 +47,33 @@ class MainActivity : AppCompatActivity() {
 
     private fun onPermissionResult(granted: Boolean) {
         if (granted) {
-            start()
+            startScanner()
         } else {
             finish()
         }
     }
 
-    private fun start() {
+    private fun startScanner() {
         codeScanner.start()
     }
 
     private fun onDetectCode(codes: List<Barcode>) {
-//        codes.forEach {
-//            Toast.makeText(this, it.rawValue, Toast.LENGTH_LONG).show()
-//        }
-        codes.forEach{
-            println("raw value ${it.rawValue}")
-            val response = fetchBookInfo(it.rawValue as String)
-            parseXml(response)
-            // parsexmlしたらcodesをリセットしたい
+        if (isOpenDialog) return
+        for (code in codes) {
+            println("raw value ${code.rawValue}")
+            if (isIsbn(code.rawValue as String)) {
+                val responseBody = fetchBookInfo(code.rawValue as String)?.body()
+                parseXml(responseBody)
+                // parse xmlしたらcodesをリセットしたい
+                break
+            }
         }
     }
-    fun fetchBookInfoFromIsbn(isbn: String): Call<ResponseBody> {
+    private fun isIsbn(code: String): Boolean {
+        if (code.length != 13) return false
+        return  code.substring(0, 3) == "978"
+    }
+    private fun fetchBookInfoFromIsbn(isbn: String): Call<ResponseBody> {
         val query = """
             isbn="$isbn"
         """.trimIndent()
@@ -84,14 +90,15 @@ class MainActivity : AppCompatActivity() {
 
         return get.execute()
     }
-    private fun parseXml(response: Response<ResponseBody>) {
-        val responseBody: ResponseBody? = response?.body()
+    private fun parseXml(responseBody: ResponseBody?) {
         val issNdlBookInfoList: List<IssNdlXmlParser.IssNdlBookInfo>? = responseBody?.byteStream()?.use { stream ->
             println("stream: $stream")
             IssNdlXmlParser().parse(stream)
         }
         println("entries")
-        createConfirmDialog(issNdlBookInfoList?.firstOrNull()?.title ?: "")
+        if (issNdlBookInfoList?.firstOrNull()?.title != null) {
+            createConfirmDialog(issNdlBookInfoList?.firstOrNull())
+        }
 //            val numOfBookInfoProperties
 //            val cityWithMaxDegrees = issNdlBookInfoList.maxByOrNull { it. }
             // filterしたかった
@@ -111,21 +118,28 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun createConfirmDialog(message: String) {
+    private fun createConfirmDialog(bookInfo: IssNdlXmlParser.IssNdlBookInfo?) {
+        isOpenDialog = true
         val alertDialog: AlertDialog? = this@MainActivity.let {
+            val title = bookInfo?.title
             val builder = AlertDialog.Builder(it)
-            builder.setTitle("notionにこの本の情報を登録しますか？${message}")
+            builder.setTitle("notionにこの本の情報を登録しますか？${title}")
             builder.apply {
                 setPositiveButton(android.R.string.ok,
                     DialogInterface.OnClickListener { dialog, id ->
                         // User clicked OK button
-                        println("click ok button, message: $message")
+                        println("click ok button, message: $title")
+                        dialog.dismiss()
+                        // NOTE: dialogを閉じるのでscan再開
+                        isOpenDialog = false
                     })
                 setNegativeButton(android.R.string.cancel,
                     DialogInterface.OnClickListener { dialog, id ->
                         // User cancelled the dialog
                         println("click cancel button")
                         dialog.cancel()
+                        // NOTE: dialogを閉じるのでscan再開
+                        isOpenDialog = false
                     })
             }
             // Set other dialog properties
